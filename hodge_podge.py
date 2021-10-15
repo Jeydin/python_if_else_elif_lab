589b179278a534a9b3b5e8caf7508548612ad078
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-10-15 16:25:26 +0000

    Finished the lab

--- hodge_podge.py
+++ hodge_podge.py
@@ -1,58 +1,58 @@
 def is_multiple_of(x, y):
 	if(x % y == 0):
-		print("True")
+		return "True"
 	else:
-		print("False")
+		return "False"
 
-is_multiple_of(12, 5)
+print(is_multiple_of(12, 5))
 
 def is_leap_year(x):
 	if(x % 100 == 0):
 		if(x % 400 == 0):
-			print("True")
+			return "True"
 	elif(x % 4 == 0):
-		print("True")
+		return "True"
 	else:
-		print("False")
+		return "False"
 
-is_leap_year(2016)
+print(is_leap_year(2016))
 
 def determine_quadrant(x, y):
 	if(x == 0 and y == 0):
-		print("Origin")
+		return "Origin"
 	if(x > 0 and y > 0):
-		print("I")
+		return "I"
 	elif(x < 0 and y > 0):
-		print("II")
+		return "II"
 	elif(x < 0 and y < 0):
-		print("III")
+		return "III"
 	elif(x > 0 and y < 0):
-		print("IV")
+		return "IV"
 	elif(x > 0 and y == 0):
-		print("+ x-axis")
+		return "+ x-axis"
 	elif(x < 0 and y == 0):
-		print("- x-axis")
+		return "- x-axis"
 	elif(x == 0 and y > 0):
-		print("+ y-axis")
+		return "+ y-axis"
 	elif(x == 0 and y < 0):
-		print("- y-axis")
+		return "- y-axis"
 
-determine_quadrant(3, 0)
+print(determine_quadrant(3, 0))
 
 def calc_federal_income_tax(x):
 	if(x >= 0 and x <= 9225):
-		print(x * .1)
+		return x * .1
 	elif(x >= 9226 and x <= 37450):
-		pass
+		return 922.5 + (x - 9225) * .15
 	elif(x >= 37451 and x <= 90750):
-		pass
+		return 5156.25 + (x - 37450) * .25
 	elif(x >= 90751 and  x <= 189300):
-		pass
+		return 18481.25 + (x - 90750) * .25
 	elif(x >= 189301 and x <= 411500):
-		pass
+		return 46075.25 + (x - 189300) * .33
 	elif(x >= 411501 and x <= 413200):
-		pass
+		return 119401.25 + (x - 441500) * .35
 	elif(x >= 413201):
-		pass
+		return 119996.25 + (x - 413200) * .396
 
-calc_federal_income_tax(5000)
+print(calc_federal_income_tax(50000))
